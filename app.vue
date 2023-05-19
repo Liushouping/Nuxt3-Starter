072f7c42e762c6ce4915d57e580921ec512a3653
liushouping <kose146352@gmail.com>
2023-05-19 14:19:31 +0800

    Nuxt3 Starter

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator color="#ff5500"/>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  setup() {
+
+		useHead({
+		  title: 'title',
+		  meta: [
+		    { name: 'description', content: 'title' }
+		  ],
+		})
+    return {
+
+    }
+  }
+}
+</script>
+<template>
+	<div>
+		Hello
+	</div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+  @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
+  
+  body,html {
+  	font-family: 'Anton', sans-serif;
+  };
+
+</style>
